04503c8875612ddc870bd41084c42f01e92561b4
Seth Wax <sethlwax@gmail.com>
2018-04-05 02:13:14 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name, :posts
+  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -9,10 +10,16 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     add_post(post)
   end
+
+  def self.post_count
+    @@post_count
+  end
+
 end
